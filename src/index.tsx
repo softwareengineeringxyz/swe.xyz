67ff4d4a5ba8e9741c133f5238c26f4fd705bba5
Amir <amirmahdi76@gmail.com>
2021-08-08 01:08:29 -0700

    Rename components. Update logo. Cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import * as React from "react";
 import ReactDOM from "react-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
-import WithSubnavigation from "./WithSubnavigation";
-import LargeWithAppLinksAndSocial from "./LargeWithAppLinksAndSocial";
+import Navigation from "./Navigation";
+import Footer from "./Footer";
 import CallToActionWithAnnotation from "./CallToActionWithAnnotation";
 
 ReactDOM.render(
   <ChakraProvider>
-    <WithSubnavigation />
+    <Navigation />
     <CallToActionWithAnnotation />
-    <LargeWithAppLinksAndSocial />
+    <Footer />
   </ChakraProvider>,
   document.getElementById("root")
 );
